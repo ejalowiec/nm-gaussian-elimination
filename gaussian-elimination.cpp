72f158057e15c9817f072c270007d743b2f0157c
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2020-12-17 15:54:51 +0100

    added little adjustments to the code

--- gaussian-elimination.cpp
+++ gaussian-elimination.cpp
@@ -37,44 +37,42 @@ int main() {
 
 	file.close();
 
-	double* tempArr = new double[size];
+	double* resultArr = new double[size];
 
 	// displaying initial equations
-	cout << "Equations: " << endl;
 	cout << "No of equations: " << size << endl;
+	cout << "Equations: " << endl;
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j <= size; ++j)
 			cout << arr[i][j] << setw(12);
 		cout << "\n";
 	}
 
-	// pivotisation
-	for (int i = 0; i < size; ++i) {
-		for (int k = i + 1; k < size; ++k) {
-			if (abs(arr[i][i]) < abs(arr[k][i])) {
-				for (int j = 0; j <= size; ++j) {
-					double temp = arr[i][j];
-					arr[i][j] = arr[k][j];
-					arr[k][j] = temp;
-				}
-			}
-		}
-	}
-	cout << "\nMatrix after pivotisation:\n";
+	//// pivotisation
+	//for (int i = 0; i < size; ++i) {
+	//	for (int k = i + 1; k < size; ++k) {
+	//		if (abs(arr[i][i]) < abs(arr[k][i])) {
+	//			for (int j = 0; j <= size; ++j) {
+	//				double temp = arr[i][j];
+	//				arr[i][j] = arr[k][j];
+	//				arr[k][j] = temp;
+	//			}
+	//		}
+	//	}
+	//}
+	//cout << "\nMatrix after pivotisation:\n";
 
-	// matrix after pivotisation
-	for (int i = 0; i < size; ++i) {
-		for (int j = 0; j <= size; ++j)
-			cout << arr[i][j] << setw(12);
-		cout << "\n";
-	}
+	//// matrix after pivotisation
+	//for (int i = 0; i < size; ++i) {
+	//	for (int j = 0; j <= size; ++j)
+	//		cout << arr[i][j] << setw(12);
+	//	cout << "\n";
+	//}
 
 	// gauss elimination
 	for (int i = 0; i < size - 1; i++) {
 		for (int k = i + 1; k < size; ++k) {
 			double ratio = arr[k][i] / arr[i][i];
-			cout << ratio << endl;
-			//make the elements below the pivot elements equal to zero or eliminate the variables
 			for (int j = 0; j <= size; ++j)
 				arr[k][j] -= (ratio * arr[i][j]);
 		}
@@ -87,31 +85,23 @@ int main() {
 		cout << "\n";
 	}
 
-	//back-substitution
-
-	//x is an array whose values correspond to the values of x,y,z..
-	for (int i = size - 1; i >= 0; i--)	{
-		//make the variable to be calculated equal to the rhs of the last equation
-		tempArr[i] = arr[i][size];
-		for (int j = i + 1; j < size; ++j)
-			//then subtract all the lhs values except the coefficient of the variable whose value is being calculated
+	// back substitution
+	for (int i = size - 1; i >= 0; --i)	{
+		resultArr[i] = arr[i][size];
+		for (int j = i + 1; j < size; ++j) {
 			if (j != i)
-				tempArr[i] = tempArr[i] - arr[i][j] * tempArr[j];
-		//now finally divide the rhs by the coefficient of the variable to be calculated
-		tempArr[i] = tempArr[i] / arr[i][i];
+				resultArr[i] -= (arr[i][j] * resultArr[j]);
+		}
+		resultArr[i] /= arr[i][i];
 	}
-	cout << "\nThe values of the variables are as follows:\n";
+
+	// print result
+	cout << "\nResult:\n";
 	for (int i = 0; i < size; ++i)
-		cout << tempArr[i] << endl;            // Print the values of x, y,z,....    
-	
+		cout << "x" << i + 1 << ":\t" << resultArr[i] << endl;
 
 
-	cout << "\nMatrix: " << endl;
-	for (int i = 0; i < size; ++i) {
-		for (int j = 0; j < size + 1; ++j)
-			cout << arr[i][j] << setw(12);
-		cout << "\n";
-	}
+	// delete dynamic arrays
 
 	for (int i = 0; i < size; ++i) {
 		delete[] arr[i];
